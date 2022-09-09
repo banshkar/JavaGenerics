82cf2890572d6e1cfcff07a0b532d26521ade6fc
jiendra <jitendra@banshkar.com>
2022-09-09 06:46:31 +0530

    Refactor 1

--- Array.java
+++ Array.java
@@ -1,34 +1,10 @@
-public class Array{
-   public static int maximumValue(Integer[]array){
-       int max = array[0];
-       for(int index = 0; index<array.length; index++){
-           if(array[index]>max){
-               max =array[index];
-           }
-       }
-       return max;
-   }
-    public static Double maximumValue(Double[]array){
-        Double max = array[0];
-        for(int index = 0; index<array.length; index++){
-            if(array[index]>max){
-                max =array[index];
-            }
-        }
-        return max;
-    }
-    public static String maximumValue(String[]array){
-        String max = array[0];
-        for(int index = 0; index<array.length; index++){
-            if(array[index].length()>max.length()){
-                max =array[index];
-            }
-        }
-        return max;
-    }
+public class Array<T extends Comparable<T>>{
+     T value_1;
+     T value_2;
+     T value_3;
     public static void main(String[] args) {
-      String[]strings ={"rahul","jitendra","sonu"};
-      String max = Array.maximumValue(strings);
-        System.out.println("the maximum String is : "+ max);
+        Array <Integer> array_1 = new Array<>();
+        Array <Double> array_2 = new Array<>();
+        Array <String> array_3 = new Array<>();
     }
 }
