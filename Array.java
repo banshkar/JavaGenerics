9b5497f728368f2c42c41e842babe9e9170954ae
jiendra <jitendra@banshkar.com>
2022-09-09 06:11:42 +0530

    UC_2

--- Array.java
+++ Array.java
@@ -8,9 +8,19 @@ public class Array{
        }
        return max;
    }
+    public static Double maximumValue(Double[]array){
+        Double max = array[0];
+        for(int index = 0; index<array.length; index++){
+            if(array[index]>max){
+                max =array[index];
+            }
+        }
+        return max;
+    }
+
     public static void main(String[] args) {
-       Integer[]integers = {80,26,50};
-       int max = Array.maximumValue(integers);
+      Double[]doubles = {80.5,26.5,50.3};
+       Double max = Array.maximumValue(doubles);
         System.out.println("the maximum Value :"+ max);
     }
 }
